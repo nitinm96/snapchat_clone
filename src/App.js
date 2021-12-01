734b4498e2d072f332270a9035a18c5325d2b96a
nitinm96 <nitin.minhas@hotmail.com>
2021-12-01 18:45:13 -0500

    fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './App.css';
+import './style/App.css';
 import WebcamCapture from './WebcamCapture';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Preview from './Preview';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets Build Snapchat!</h1>
-      <WebcamCapture  />
+    <div className="app">
+      <Router>
+        <div className = "app__body">
+          <Switch>
+            <Route path="/Preview" >
+              <Preview />
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture  />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
