73fb4b4764fa4f84377a246db4f90bb6e606e895
nitinm96 <nitin.minhas@hotmail.com>
2021-12-01 13:01:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WebcamCapture from './WebcamCapture';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets Build Snapchat!</h1>
+      <WebcamCapture  />
+    </div>
+  );
+}
+
+export default App;
